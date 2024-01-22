bdf73f6ea0e5a5c1e6a797cb25650e1bdaec5ba2
Tekknobot <thrillatron@hotmail.com>
2024-01-22 00:47:02 -0500

    Looks good

--- src/StructureManager.gd
+++ src/StructureManager.gd
@@ -10,5 +10,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
-	var tile_pos = get_node("../TileMap").local_to_map(self.position)	
-	self.z_index = tile_pos.x + tile_pos.y
+	#var tile_pos = get_node("../TileMap").local_to_map(self.position)	
+	#self.z_index = tile_pos.x + tile_pos.y
+	pass
