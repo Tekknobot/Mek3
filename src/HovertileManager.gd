c7307084699f81152415ec639a2db7ae08d0c481
Tekknobot <thrillatron@hotmail.com>
2024-02-17 14:25:29 -0500

    Use hover tile script.

--- src/HovertileManager.gd
+++ src/HovertileManager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var tile_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	self.tile_pos = get_node("../TileMap").local_to_map(self.position)
+	# Z index layering
+	self.z_index = tile_pos.x + tile_pos.y
